c5d951fe3239b039185a31be781bfe3cb2e3c5f0
Àlex Alemany <86841068+aleny23@users.noreply.github.com>
2026-08-06 15:55:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  *
  * This file rarely needs to change. Bump CACHE only if you change the SW logic.
  */
-const CACHE = 'prsonal-shell-v25';
+const CACHE = 'prsonal-shell-v27';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
